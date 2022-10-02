073f2312c42c34f77c3eab320bb0613e9f4986ee
reaper <reaper>
2022-10-02 12:43:03 +0300

    homework/1

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+string number1 = Console.ReadLine();
+int a = int.Parse(number1);
+Console.Write("Введите второе число: ");
+string number2 = Console.ReadLine();
+int b = int.Parse(number2);
+
+int max = a;
+
+if(a>max) max=a;
+if(b>max) max=b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
